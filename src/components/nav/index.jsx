2643793ed9b22d52d87552d826353493a3b694b1
Lokesh Jain <lokesh.jain@connectwise.com>
2021-06-17 19:53:20 +0530

    Updated nav compoent

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -9,19 +9,19 @@ const vAccordion = "accordion";
 
 function Nav(props) {
     const { title, subtitle, color = "Red", variant = vNavbar,
-        className, style = {}, children, ...rest } = props;
+        elevated = true, className, style = {}, children, ...rest } = props;
 
     const [active, setActive] = React.useState(false);
     const handleClick = () => setActive(!active);
 
     return <nav style={{ ...Colors[color], ...style }}
-        className={["jn-nav", className, variant].join(' ')} {...rest}>
+        className={["jn-nav", className, elevated ? "jn-elevated" : "", variant].join(' ')} {...rest}>
 
         {title &&
             <header className={["jn-nav-header", variant].join(' ')}>
                 <div className="title">{title}</div>
                 {subtitle && <div className="subtitle">{subtitle}</div>}
-                <div className="action" onClick={handleClick}>A</div>
+                <div className="action" onClick={handleClick}>≡</div>
             </header>
         }
 
@@ -32,11 +32,12 @@ function Nav(props) {
 }
 
 Nav.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
     color: PropTypes.oneOf(Colors.Values),
     variant: PropTypes.oneOf([vNavbar, vAccordion]),
     style: PropTypes.object,
+    elevated:PropTypes.bool,
 };
 
 const vItem = "item";
@@ -44,8 +45,8 @@ const vLink = "link";
 const vMenu = "menu";
 
 function NavItem(props) {
-    const { to, label, icon, variant = vItem,
-        className, children, ...rest } = props;
+    const { to, label, icon, variant = vLink,
+        elevated = true, className, children, ...rest } = props;
 
     const [open, setOpen] = React.useState(false);
     const handleEnter = () => setOpen(true);
@@ -55,35 +56,45 @@ function NavItem(props) {
     const size = useWindowSize();
 
     if (variant === vLink) {
-        return <Link className={["jn-nav-link", className].join(' ')} to={to} {...rest}>
+        return <Link className={["jn-nav-link", "jn-nav-item", "jn-nav-divider", className].join(' ')} to={to} {...rest}>
             {icon} {label}
         </Link>
     }
 
+
+
     if (variant === vMenu && size.width > 620) {
-        return <div className="jn-sub-nav" {...rest} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
-            <button className={["jn-nav-btn", className, open ? "active" : ""].join(' ')}>{icon} {label}</button>
-            {open && children && <div className="jn-sub-nav-content"> {children}</div>}
+        return <div className={["jn-sub-nav", "jn-nav-divider", className].join(' ')} {...rest} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+            <button className={["jn-nav-btn", "jn-nav-item", open ? "active" : ""].join(' ')}>{icon} {label}</button>
+            {open && children &&
+                <div className={["jn-sub-nav-content", elevated ? "jn-elevated" : "", open ? "active" : ""].join(' ')}>
+                    {children}
+                </div>
+            }
         </div>;
     }
 
     if ((variant === vMenu && size.width <= 620) || variant === vAccordion) {
-        return <div className={["jn-sub-nav", className].join(' ')} {...rest}>
-            <button className={["jn-nav-btn", open ? "active" : ""].join(' ')} onClick={handleClick}>{icon} {label}</button>
-            {open && children && <div className="jn-sub-nav-content"> {children}</div>}
+        return <div className={["jn-sub-nav", "jn-nav-divider", className].join(' ')} {...rest}>
+            <button className={["jn-nav-btn", "jn-nav-item", open ? "active" : ""].join(' ')} onClick={handleClick}>{icon} {label}</button>
+            {open && children &&
+                <div className={["jn-sub-nav-content", elevated ? "jn-elevated" : "", open ? "active" : ""].join(' ')}>
+                    {children}
+                </div>
+            }
         </div>;
     }
-
 
     return <div className={className}> {children} </div>;
 }
 
 NavItem.propTypes = {
     to: PropTypes.string,
-    label: PropTypes.string,
+    label: PropTypes.string.isRequired,
     icon: PropTypes.node,
     variant: PropTypes.oneOf([vItem, vLink, vMenu, vAccordion]),
     className: PropTypes.string,
+    elevated:PropTypes.bool,
 }
 
 function useWindowSize() {
--- src/view/playground/index.jsx
+++ src/view/playground/index.jsx
@@ -1,54 +1,84 @@
+import { Home } from "@material-ui/icons";
 import React from "react";
+import Colors from '../../components/colors'
+import { Nav, NavItem } from '../../components/nav'
+
 import './playground.css'
 
+const vVariant = "menu";//"accordion"; //
+const vVariantNav = "navbar"; //"accordion"; //
+
 export default function Playground(props) {
-    return <div className="jn-gallery">
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry
-        </div>
-        <div className="jn-gallery-item">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry
-        </div>
-    </div>
+    return <Nav title="Nav Bar" subtitle="Nav Bar Example" variant={vVariantNav}>
+        <NavItem to="#Home" label="Home" variant="link" icon={<Home />} />
+        <NavItem to="#About" label="About" variant={vVariant} icon={<Home />}>
+            <NavItem to="#Company" label="Company" variant="link" />
+            <NavItem to="#Team" label="Team" variant="link" />
+            <NavItem to="#Careers" label="Careers" variant="link" />
+        </NavItem>
+        <NavItem to="#Services" label="Services" variant={vVariant}>
+            <NavItem to="#Bring" label="Bring" variant="link" />
+            <NavItem to="#Deliver" label="Deliver" variant={vVariant}>
+                <NavItem to="#Bring" label="Food" variant="link" />
+                <NavItem to="#Bring" label="Hardware" variant="link" />
+                <NavItem to="#Bring" label="Electronics" variant="link" />
+            </NavItem>
+            <NavItem to="#Package" label="Package" variant="link" />
+            <NavItem to="#Express" label="Express" variant="link" />
+        </NavItem>
+        <NavItem to="#Partners" label="Partners" variant="link" />
+        <NavItem to="#Contact" label="Contact" variant="link" />
+    </Nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <nav className="jn-navbar" style={Colors["Grey"]}>
+    //     <a href="#home">Home</a>
+    //     <div class="subnav">
+    //         <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
+    //         <div class="subnav-content">
+    //             <a href="#company">Company</a>
+    //             <a href="#team">Team</a>
+    //             <div class="subnav">
+    //                 <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
+    //                 <div class="subnav-content">
+    //                     <a href="#company">Company</a>
+    //                     <a href="#team">Team</a>
+    //                     <a href="#careers">Careers</a>
+    //                 </div>
+    //             </div>
+    //             <a href="#careers">Careers</a>
+    //         </div>
+    //     </div>
+    //     <div class="subnav">
+    //         <button class="subnavbtn">Services <i class="fa fa-caret-down"></i></button>
+    //         <div class="subnav-content">
+    //             <a href="#bring">Bring</a>
+    //             <a href="#deliver">Deliver</a>
+    //             <a href="#package">Package</a>
+    //             <a href="#express">Express</a>
+    //         </div>
+    //     </div>
+    //     <div class="subnav">
+    //         <button class="subnavbtn">Partners <i class="fa fa-caret-down"></i></button>
+    //         <div class="subnav-content">
+    //             <a href="#link1">Link 1</a>
+    //             <a href="#link2">Link 2</a>
+    //             <a href="#link3">Link 3</a>
+    //             <a href="#link4">Link 4</a>
+    //         </div>
+    //     </div>
+    //     <a href="#contact">Contact</a>
+    //     <div style={{ clear: "both" }}></div>
+    // </nav>
 }
